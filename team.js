50f8c8fc42b3a7bb44f4a68f3fe3f00803cf25a5
RexWithLuv <aaronaax@gmail.com>
2024-02-05 19:14:47 +0100

    Página equipo funcionando con el botón, no permitiremos almacenar todo el equipo (total pa que).
    Falta examinación de Ana.

--- team.js
+++ team.js
@@ -4,4 +4,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Mostrar el número aleatorio en el campo correspondiente
   document.getElementById("trainer-id").value = numeroAleatorio;
+
+  const formularioTarjeta = document.getElementById("formularioTarjeta");
+  const boton = document.getElementById("boton");
+
+  boton.addEventListener("click", function () {
+    // Verificar si todos los campos están completos
+    const nombre = document.getElementById("nombre").value;
+    const pokemonElegido = document.getElementById("pokemonElegido").value;
+
+    if (nombre.trim() !== "" && pokemonElegido !== "null") {
+      // Si todos los campos están completos, ejecutar la función mensajeSaved
+      mensajeSaved();
+    } else {
+      // Mostrar un mensaje de error o realizar alguna acción si algún campo está vacío
+      alert("Por favor, completa todos los campos antes de almacenar.");
+    }
+  });
+
+  function mensajeSaved() {
+    const messageElement = document.getElementById("mensaje");
+    messageElement.textContent = "Equipo guardado! ^^";
+    messageElement.style.opacity = "1";
+    messageElement.style.display = "block";
+    messageElement.classList.add("animated");
+
+    setTimeout(() => {
+      messageElement.style.opacity = "0"; // Desvanecer
+      setTimeout(() => {
+        messageElement.textContent = "";
+        messageElement.style.display = "none";
+      }, 1500); // Esperar a que termine la transición
+    }, 4500); // Mostrar durante 4 segundos (puedes ajustar este valor)
+  }
 });
